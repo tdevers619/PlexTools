3df8eb802d71a0225bab2fa7b014a4bec7cfbb1e
Tyler Devers <72813823+tdevers619@users.noreply.github.com>
2024-01-05 10:48:19 -0800

    Add files via upload

---
+++ autoUnzip.ps1
@@ -0,0 +1,17 @@
+$7zipPath = "C:\Path\To\7zip\7z.exe"
+$sourceFolder = "C:\Path\To\Source\Folder"
+$destinationFolder = "C:\Path\To\Extract\Destination"
+$minFileSizeMB = 50  # Set the minimum file size in megabytes
+
+# Get all zip files in the source folder with a size greater than or equal to the specified minimum size
+$zipFiles = Get-ChildItem -Path $sourceFolder -Filter *.zip | Where-Object { $_.Length -ge ($minFileSizeMB * 1MB) }
+
+foreach ($zipFile in $zipFiles) {
+    $zipFilePath = $zipFile.FullName
+
+    # Construct the 7zip command to extract the zip file to the destination folder
+    $extractCommand = "$7zipPath x `"$zipFilePath`" -o`"$destinationFolder`""
+
+    # Run the 7zip command
+    Invoke-Expression $extractCommand
+}
