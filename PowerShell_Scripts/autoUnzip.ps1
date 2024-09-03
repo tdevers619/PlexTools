3d52c7c8b7ba1d3acb7a9d209aee44e0a9bd5355
Tyler Devers <72813823+tdevers619@users.noreply.github.com>
2024-09-03 16:28:23 -0700

    Fixed .rar file compatability and adjusted file size for proper detection

--- PowerShell_Scripts/autoUnzip.ps1
+++ PowerShell_Scripts/autoUnzip.ps1
@@ -1,10 +1,10 @@
 $7zipPath = "C:\Path\To\7zip\7z.exe"
 $sourceFolder = "C:\Path\To\Source\Folder"
 $destinationFolder = "C:\Path\To\Extract\Destination"
-$minFileSizeMB = 50  # Set the minimum file size in megabytes
+$minFileSizeMB = 150  # Set the minimum file size in megabytes
 
 # Get all zip files in the source folder with a size greater than or equal to the specified minimum size
-$zipFiles = Get-ChildItem -Path $sourceFolder -Filter *.zip | Where-Object { $_.Length -ge ($minFileSizeMB * 1MB) }
+$zipFiles = Get-ChildItem -Path $sourceFolder -Filter *.zip, *.rar | Where-Object { $_.Length -ge ($minFileSizeMB * 1MB) }
 
 foreach ($zipFile in $zipFiles) {
     $zipFilePath = $zipFile.FullName
